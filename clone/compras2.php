4de226c88a7407a79366eb19de3ee418eba36e5d
Sara Do Carmo <sara.carmo@alunos.sc.senac.br>
2022-09-03 20:18:00 -0300

    feita tela de compra

--- clone/compras2.php
+++ clone/compras2.php
@@ -1,19 +1,50 @@
 <?php
-include "head2.php"
+include "head.php"
 ?>
+<div class="container-produto">
+    <div class="container-compras">
+        <div class="container-image">
+            <img src="assets/img/.jpg">
+        </div>
+        <div class="container-informacoes">
 
-<div class="compra">
-    <div class="img">
-        <img src="../assets/img/cinza.jpg">
-    </div>
-    <div class="direita">
-        <h1>MESA </h1>
-        <P>descrição</P>
-        <h2>valor</h2>
+            <h1>NOME</h1>
+            <div class="tamanho-cor">
+                <p>Cor</p>
+            </div>
+            <div class="tamanho-tm">
+                <p>Tamanho</p>
+            </div>
+            <div class="tamanho-material">
+                <p>Material</p>
+            </div>
+            <div class="tamanho-descricao">
+                <p>orem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor i</p>
+            </div>
+            <div class="container-botao">
+                <a href="https://api.whatsapp.com/send?phone=554799255732" target="_blank"><input type="submit" value="Fale direto com o Vendedor"></a>
+            </div>
+        </div>
     </div>
 </div>
 
 
+<div class="outros-produtos">
+    <div class="mais-produtos">
+        <h2>MAIS PRODUTOS</h2>
+    </div>
+    <div class="container-outras-imgs">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    <img src="assets/img/.jpg">
+    </div>
+</div>
+
 <?php
-include "footer2.php"
+include "footer.php"
 ?>
