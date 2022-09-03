494aa85cb0d8d93dfb53704dc38478e2d6b3a995
Samuel De Souza <samueldesouza200512@gmail.com>
2022-09-03 13:40:39 -0300

    mudanças de organização

--- clone/prateleira2.php
+++ clone/prateleira2.php
@@ -1,44 +1,21 @@
 <?php
-include "head2.php"
-?>
+include "head2.php";
+include "../include/MySql.php";
 
+echo "<h1 class='espacos-h1'>PRATELEIRAS</h1>";
+echo "<section class='espacos-class'>";
+$sql = $pdo->prepare('SELECT * FROM PRODUTOS WHERE idclasse LIKE "1" ');
+if ($sql->execute()) {
+    $info = $sql->fetchAll(PDO::FETCH_ASSOC);
+    foreach ($info as $key => $value) {
+    echo "<div>";
+    $imagem = $value["imagem"];
+    echo '<a href="#"><img style= "width:300px;"src="data:image/jpg;charset=utf8;base64,' . base64_encode($imagem) . '"></td>';
+    echo '<h2>'.$value["nome"].'</h2>';
+    echo "</div>";   
+    }
+}
+echo "</section>";
 
-<h1 class="espacos-h1">PRATELEIRAS</h1>
-
-<section class="espacos-class">
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-    <div>
-    <a href="#"><img src="../assets/img/cinza.jpg" >
-    <p>#################</p>
-    <h2>R$ ###,##</h2></a>
-    </div>
-
-</section>
-
-<?php
 include "footer2.php"
 ?>
--- decoracoes.php
+++ decoracoes.php
@@ -17,10 +17,6 @@ if ($sql->execute()) {
     }
 }
 echo "</section>";
-?>
 
-
-
-<?php
 include "footer.php"
 ?>
