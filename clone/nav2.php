c05f1aec254ab86c95b444942528cb3b1e0c60de
Samuel de Souza <samueldesouza200517@gmail.com>
2022-09-08 16:06:49 -0300

    mudanças

--- clone/nav2.php
+++ clone/nav2.php
@@ -24,7 +24,6 @@
             <p>Entre em contato</p>
             <div class="sub-menu-1">
                 <ul>
-                    <li><a href="topferroatendimento@gmail.com" target="_blank" rel="noopener noreferrer"><img src="../assets/img/gmail.png">Email</a> </li>
                     <li><a href="https://www.instagram.com/topferrojoinville/" target="_blank"> <img src="../assets/img/instagram.png">Instagram</a></li>
                     <li> <a href="https://api.whatsapp.com/send?phone=554799255732" target="_blank"><img src="../assets/img/whatsapp.png">Whatsapp</a></li>
                     <li> <a href="https://www.facebook.com/estruturaparamoveis/" target="_blank"><img src="../assets/img/facebook.png">Facebook</a></li>
